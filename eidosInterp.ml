5edba9b1c60e3ee677c203c83d96607c27827315
Bosco Ndemeye <ndemeyemm@hendrix.edu>
2018-11-16 19:08:03 -0800

    add interp func skeleton

---
+++ eidosInterp.ml
@@ -0,0 +1,87 @@
+open EidosAST
+
+exception Undefined
+
+let rec interpBlock = raise Undefined
+
+let rec interpStatement = raise Undefined
+
+let rec interpCompoundStmt = raise Undefined
+
+let rec interpExprStmt = raise Undefined
+
+let rec interpSelectStmt = raise Undefined
+
+let rec interpForStmt = raise Undefined
+
+let rec interpDoWhileStmt = raise Undefined
+
+let rec interpWhileStmt = raise Undefined
+
+let rec interpJumpStmt = raise Undefined
+
+let rec interpExpr = raise Undefined
+
+let rec interpAssignExpr = raise Undefined
+
+let rec interpConditionalExpr = raise Undefined
+
+let rec interpLorExpr = raise Undefined
+
+let rec interpLAndExpr = raise Undefined
+
+let rec interpEqtExpr = raise Undefined
+
+let rec interpEqNeqExpr = raise Undefined
+
+let rec interpRelExpr = raise Undefined
+
+let rec interpComparisonExpr = raise Undefined
+
+let rec interpAddExpr = raise Undefined
+
+let rec interpAddSubMulExpr = raise Undefined
+
+let rec interpMultExpr = raise Undefined
+
+let rec interpMulDivSeqExpr = raise Undefined
+
+let rec interpSeqExpr = raise Undefined
+
+let rec interpExpExpr = raise Undefined
+
+let rec interpUnaryExpr = raise Undefined
+
+let rec interpPostFixExpr = raise Undefined
+
+let rec interpFuncExec = raise Undefined
+
+let rec interpFuncDefn = raise Undefined
+
+let rec interpAttributeAccessor = raise Undefined
+
+let rec interpIndexing = raise Undefined
+
+let rec interpConstant = raise Undefined
+
+let rec interpIdentifier = raise Undefined
+
+let rec interpPrimaryExpr = raise Undefined
+
+let rec interpArgumentExpr = raise Undefined
+
+let rec interpFuncDecl = raise Undefined
+
+let rec interpReturnTypeSpec = raise Undefined
+
+let rec interpTypeSpec = raise Undefined
+
+let rec interpTypeSpecH = raise Undefined
+
+let rec interpTyAbrev = raise Undefined
+
+let rec interpObjectClassSpec = raise Undefined
+
+let rec interpParamList = raise Undefined
+
+let rec interpParamSpec = raise Undefined
